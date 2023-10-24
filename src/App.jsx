2a5531c5c46567985ec2e0c3811525607a6557a5
ashishkhanagwal20 <ashishstudy20@gmail.com>
2023-10-24 21:08:17 +0530

    phonebook2.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   // const addPerson = (event) => {
@@ -28,10 +33,12 @@ const App = () => {
     } else {
       const personObj = {
         name: newName,
+        number: newNumber,
       };
 
       setPersons(persons.concat(personObj));
       setNewName("");
+      setNewNumber("");
     }
   };
   return (
@@ -41,6 +48,9 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
+        <div>
+          Number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
         <div>debug: {newName}</div>
         <div>
           <button>add</button>
@@ -49,7 +59,9 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((p) => (
-          <li key={p.name}>{p.name}</li>
+          <li key={p.name}>
+            {p.name} {p.number}
+          </li>
         ))}
       </ul>
     </div>
